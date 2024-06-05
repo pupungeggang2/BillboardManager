80388ef2f9346db3531d03eda8915369c2e731fe
pupungeggang2 <kyongkyong2892@gmail.com>
2024-06-05 11:00:52 +0900

    0.0.1

--- Server/app.js
+++ Server/app.js
@@ -5,32 +5,32 @@ const app = express();
 var connection = []
 
 const httpServer = app.listen(3001, () => {
-  console.log("3001");
-});
+    console.log("3001")
+})
 
 const webSocketServer = new ws.Server({
-  server: httpServer,
-});
+server: httpServer,
+})
 
 webSocketServer.on("connection", (ws, request) => {
-  const ip = request.socket.remoteAddress;
+const ip = request.socket.remoteAddress
 
-  console.log(`Server started at ${ip}`);
+console.log(`Server started at ${ip}`)
 
-  if (ws.readyState === ws.OPEN) {
-    console.log(`[${ip}]`);
-  }
+if (ws.readyState === ws.OPEN) {
+console.log(`[${ip}]`)
+}
 
-  ws.on("message", (msg) => {
-    console.log(`${msg} [${ip}]`);
-    ws.send(`${msg}`);
-  });
+ws.on("message", (msg) => {
+console.log(`${msg} [${ip}]`)
+ws.send(`${msg}`)
+})
 
-  ws.on("error", (error) => {
-    console.log(`${error} [${ip}]`);
-  });
+ws.on("error", (error) => {
+console.log(`${error} [${ip}]`)
+})
 
-  ws.on("close", () => {
-    console.log(`[${ip}]`);
-  });
-});
+ws.on("close", () => {
+console.log(`[${ip}]`)
+})
+})
